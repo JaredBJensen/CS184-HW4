6f992f193562f288d7480854f2f1d492de3adba7
Donghao Ren <donghao.ren@gmail.com>
2017-10-27 11:42:08 -0700

    use ImageRetriever

---
+++ app/src/main/java/edu/ucsb/cs/cs190i/samuel_dong/imagetagexplorer/ImageRetriever.java
@@ -0,0 +1,118 @@
+package edu.ucsb.cs.cs190i.samuel_dong.imagetagexplorer;
+/**
+ * Created by jalexander on 5/2/17.
+ */
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ImageRetriever {
+    private static String baseUrl = "http://ec2-52-53-191-204.us-west-1.compute.amazonaws.com:8080/";
+
+    public static void getImageList(final ImageListResultListener listener) {
+        RetrieveImageListTask retrieveImageListTask = new RetrieveImageListTask(listener);
+        retrieveImageListTask.execute();
+    }
+
+    public static void getImageByIndex(int index, final ImageResultListener listener) {
+        RetrieveImageByIndexTask retrieveTaggedImageTask = new RetrieveImageByIndexTask(listener);
+        retrieveTaggedImageTask.execute(new Integer(index));
+    }
+
+    interface ImageListResultListener {
+        void onImageList(ArrayList<String> list);
+    }
+
+    interface ImageResultListener {
+        void onImage(Bitmap image);
+    }
+
+    private static class RetrieveImageListTask extends AsyncTask<Void, Void, ArrayList<String>> {
+        private ImageListResultListener listener;
+
+        public RetrieveImageListTask(ImageListResultListener listener) {
+            super();
+            this.listener = listener;
+        }
+
+        @Override
+        protected ArrayList<String> doInBackground(Void... params) {
+            try {
+                URL pageUrl = new URL(baseUrl + "ls");
+                URLConnection connection = pageUrl.openConnection();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder pageHtmlBuilder = new StringBuilder();
+                String inputLine;
+                while ((inputLine = reader.readLine()) != null) {
+                    pageHtmlBuilder.append(inputLine);
+                    pageHtmlBuilder.append('\n');
+                }
+                reader.close();
+                String[] files = pageHtmlBuilder.toString().split("\n");
+                ArrayList<String> picNames = new ArrayList<>();
+                for (String file : files) {
+                    picNames.add(file);
+                }
+                return picNames;
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> picNames) {
+            if (listener != null) {
+                listener.onImageList(picNames);
+            }
+        }
+    }
+
+    private static class RetrieveImageByIndexTask extends AsyncTask<Integer, Void, Bitmap> {
+        private ImageResultListener listener;
+
+        public RetrieveImageByIndexTask(ImageResultListener listener) {
+            super();
+            this.listener = listener;
+        }
+
+        @Override
+        protected Bitmap doInBackground(Integer... params) {
+            try {
+                URL imageUrl = new URL(baseUrl + "img/" + params[0] + "/pic");
+                URLConnection imageConnection = imageUrl.openConnection();
+                Bitmap image = BitmapFactory.decodeStream(imageConnection.getInputStream());
+
+                URL pageUrl = new URL(baseUrl + "img/" + params[0] + "/tags");
+                URLConnection connection = pageUrl.openConnection();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder pageHtmlBuilder = new StringBuilder();
+                String inputLine;
+                while ((inputLine = reader.readLine()) != null) {
+                    pageHtmlBuilder.append(inputLine);
+                    pageHtmlBuilder.append('\n');
+                }
+                reader.close();
+                String[] tags = pageHtmlBuilder.toString().split("\n");
+                ArrayList<String> picTags = new ArrayList<String>(Arrays.asList(tags));
+                return image;
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap image) {
+            if (listener != null) {
+                listener.onImage(image);
+            }
+        }
+    }
+}
--- app/src/main/java/edu/ucsb/cs/cs190i/samuel_dong/imagetagexplorer/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs190i/samuel_dong/imagetagexplorer/MainActivity.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,24 +29,21 @@ public class MainActivity extends AppCompatActivity {
         final ImageView imageView = (ImageView)findViewById(R.id.imageView);
 
         // Although this class is called "TaggedImageRetriever", you don't have to deal with tags in this assignment.
-        TaggedImageRetriever.getNumImages(new TaggedImageRetriever.ImageNumResultListener() {
+        ImageRetriever.getImageList(new ImageRetriever.ImageListResultListener() {
             @Override
-            public void onImageNum(int num) {
-                textView.setText(textView.getText() + "\n\n" + num);
-            }
-        });
-
-        TaggedImageRetriever.getTaggedImageByIndex(0, new TaggedImageRetriever.TaggedImageResultListener() {
-            @Override
-            public void onTaggedImage(TaggedImageRetriever.TaggedImage image) {
-                if (image != null) {
-                    try (FileOutputStream stream = openFileOutput("Test.jpg", Context.MODE_PRIVATE)){
-                        image.image.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-                        image.image.recycle();
-                    } catch (IOException e) {
+            public void onImageList(ArrayList<String> list) {
+                textView.setText(String.format("Total number: %d", list.size()));
+                ImageRetriever.getImageByIndex(0, new ImageRetriever.ImageResultListener() {
+                    @Override
+                    public void onImage(Bitmap image) {
+                        try (FileOutputStream stream = openFileOutput("Test.jpg", Context.MODE_PRIVATE)){
+                            image.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                            image.recycle();
+                        } catch (IOException e) {
+                        }
+                        Picasso.with(MainActivity.this).load(getFileStreamPath("Test.jpg")).resize(500,500).centerCrop().into(imageView);
                     }
-                    Picasso.with(MainActivity.this).load(getFileStreamPath("Test.jpg")).resize(500,500).centerCrop().into(imageView);
-                }
+                });
             }
         });
     }
